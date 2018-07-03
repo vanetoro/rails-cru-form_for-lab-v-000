649bfec9d91877f39f0ea71a89555d9c349aab03
Vanessa Toro <vanetoro84@gmail.com>
2018-07-03 14:47:25 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -21,5 +21,6 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     @song.update(params.require(:song).permit(:name, :artist_id, :genre_id))
+    redirect_to song_path(@song)
   end
 end
